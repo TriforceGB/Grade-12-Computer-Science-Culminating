d120c838af847a366b190c2b2a7bd9abc1e05c26
Zachary Schultz <Zach.Schultz.2.0.1@gmail.com>
2026-06-04 10:57:49 -0400

    added the frame work for the DB

--- src/FTS.java
+++ src/FTS.java
@@ -1,12 +1,16 @@
 import UI.UI;
+import DB.DB;
 
 public class FTS {
 	// Settings
+	private static String DB_PATH = "db/FTS.db";
+
 	// Variables
 	private static UI ui;
+	private static DB db;
 
 	public static void main(String[] args) {
-		ui = new UI();
-
+		db = new DB(DB_PATH);
+		ui = new UI(db);
 	}
 }
--- src/UI/searchPanel.java
+++ src/UI/searchPanel.java
@@ -4,18 +4,24 @@ import java.util.EventListener;
 
 import javax.swing.JPanel;
 
+import DB.DB;
+
 /**
  * This page allows the user to search for media.
  */
 public class searchPanel extends JPanel implements EventListener {
 	private UI ui; // Reference to the main UI
+	private DB db; // Reference to the database
 
 	/**
 	 * Constructor for the Search Panel.
 	 *
 	 * @param ui Reference to the main UI
+	 * @param db Reference to the database
 	 */
-	public searchPanel(UI ui) {
+	public searchPanel(UI ui, DB db) {
+		this.ui = ui;
+		this.db = db;
 		// TODO
 	}
 }
