b139b3b1e5798b738522c8f20efd2e80e47c8ff9
Zachary Schultz <Zach.Schultz.2.0.1@gmail.com>
2026-06-04 14:41:01 -0400

    Query to find all tables made

--- src/DB/Query.java
+++ src/DB/Query.java
@@ -6,5 +6,14 @@ package DB;
  * Everything is static and final and can only be used inside the DB.
  */
 class Query {
-	public static final String CREATE_TABLE = ""; // TODO
+	// Meta Query
+	// Stuff that create or alter tables not Data
+
+	// This Find all Table in DB
+	public static final String FIND_TABLE = """
+	SELECT name
+	FROM sqlite_master
+	WHERE type = 'table'
+	""";
+	public static final String CREATE_TABLE = ""; //
 }
