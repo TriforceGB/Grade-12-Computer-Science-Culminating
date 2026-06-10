b39df04f6c352170246b68cbb97639b4a765bd00
Zachary Schultz <Zach.Schultz.2.0.1@gmail.com>
2026-06-10 14:09:35 -0400

    moved admin panel

---
+++ src/UI/Pages/adminPage.java
@@ -0,0 +1,15 @@
+package UI.Pages;
+
+import UI.UI;
+
+/**
+ * Admin page base for Admin Settings.
+ * Other pages in the admin settings will use this as a base.
+ */
+public class adminPage extends Page {
+	adminPage(UI ui) {
+		super(ui);
+	}
+
+	// TODO Rewrite header for the Admin Panel
+}
