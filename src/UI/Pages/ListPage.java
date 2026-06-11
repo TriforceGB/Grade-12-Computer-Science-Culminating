d0dc3168d4b181d194bd29ec3da40e9d089872c3
Michael <mrauliuc9@gmail.com>
2026-06-11 17:00:54 -0400

    List page basic setup complete

--- src/UI/Pages/ListPage.java
+++ src/UI/Pages/ListPage.java
@@ -30,11 +30,42 @@ public class ListPage extends Page {
 	// Variables
 	private JPanel contentPanel;
 	private JPanel filterPanel;
+
+	// table list variables
 	private JPanel listPanel;
 	private JTable listTable;
+	private JScrollPane tableScrollContainer;
 
-	private final String[] colNames = {"Icon", "Name", "Status", "Rating", "Last EP", "Rewatch"};
+	private final String[] colNames = { "Icon", "Name", "Status", "Rating", "Last EP", "Rewatch" };
 	private DefaultTableModel listTableModel;
+
+	private GridBagConstraints gbc;
+
+	// movie filter objects
+	private JLabel movieTypeLbl;
+	private JCheckBox movieType;
+
+	private JLabel showTypeLbl;
+	private JCheckBox showType;
+
+	private JLabel animeTypeLbl;
+	private JCheckBox animeType;
+
+	private JLabel nameFilterLbl;
+	private JTextField nameFilter;
+
+	private JLabel statusFilterLbl;
+
+	private JComboBox<String> statusFilter;
+	private String[] showStatus = new String[] { "", "Undecided", "Backlog", "Watching", "Completed", "Dropped" };
+
+	private JLabel minRatingLbl;
+	private JSpinner minRating;
+
+	private JLabel maxRatingLbl;
+	private JSpinner maxRating;
+
+	private JButton searchButton;
 
 	private final Border border = BorderFactory.createLineBorder(Color.BLACK, 2, true); // true allows for rounded
 
@@ -46,39 +77,63 @@ public class ListPage extends Page {
 	public ListPage(UI ui) {
 		super(ui); // Uses the basic page layout and background color
 
-		contentPanel = new JPanel();
-		contentPanel.setBackground(PageColor);
-		contentPanel.setLayout(new BorderLayout());
+		createContentPanel();
 
+		// add components to filter panel
+		createFilterPanel();
+
+		// add filter buttons
+		addTypeCheckboxes();
+		addNameSatusButtons();
+		addRatingSelectorButtons();
+		addSearchButton();
+
+		createListPanel();
+
+		addTableToListPanel();
+
+
+		// add content panel to main panel
+		this.add(contentPanel);
+	}
+
+	void createFilterPanel() {
 		// create filter panel
 		filterPanel = new JPanel();
 		filterPanel.setBackground(PageColor);
 		filterPanel.setLayout(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
+		gbc = new GridBagConstraints();
 		// set rounded borders
 		filterPanel.setBorder(border);
 		// then simply lock the width
 		filterPanel.setSize(new Dimension(150, 0));
+	}
 
-		// add components to filter panel
+	void createContentPanel() {
+		contentPanel = new JPanel();
+		contentPanel.setBackground(PageColor);
+		contentPanel.setLayout(new BorderLayout());
+	}
 
-		// three checkboxes in three different rows (type) (all require a label attached)
+	void addTypeCheckboxes() {
+		// three checkboxes in three different rows (type) (all require a label
+		// attached)
 		// note row comments are not accurate (psa gridy = 0 -> row 1)
-		JLabel movieTypeLbl = new JLabel("Movie: ");
+		movieTypeLbl = new JLabel("Movie: ");
 		movieTypeLbl.setFont(Style.BASE_FONT);
-		JCheckBox movieType = new JCheckBox();
+		movieType = new JCheckBox();
 
-		JLabel showTypeLbl = new JLabel("TV Show: ");
+		showTypeLbl = new JLabel("TV Show: ");
 		showTypeLbl.setFont(Style.BASE_FONT);
-		JCheckBox showType = new JCheckBox();
+		showType = new JCheckBox();
 
-		JLabel animeTypeLbl = new JLabel("Anime: ");
+		animeTypeLbl = new JLabel("Anime: ");
 		animeTypeLbl.setFont(Style.BASE_FONT);
-		JCheckBox animeType = new JCheckBox();
+		animeType = new JCheckBox();
 
 		gbc.gridx = 0; // col 1
 		gbc.gridy = 1; // row 1
-		//gbc.insets = Style.LABEL_PADS;
+		// gbc.insets = Style.LABEL_PADS;
 		filterPanel.add(movieTypeLbl, gbc);
 
 		gbc.gridx = 1; // col 2
@@ -97,19 +152,20 @@ public class ListPage extends Page {
 
 		gbc.gridx = 1; // col 2
 		filterPanel.add(animeType, gbc);
+	}
 
+	void addNameSatusButtons() {
 		// name & status row
 		// name is texfield and status is a dropdown
-		JLabel nameFilterLbl = new JLabel("Name: ");
+		nameFilterLbl = new JLabel("Name: ");
 		nameFilterLbl.setFont(Style.BASE_FONT);
-		JTextField nameFilter = new JTextField(10);
+		nameFilter = new JTextField(10);
 		nameFilter.setSize(new Dimension(100, 10));
 		nameFilter.setFont(Style.BASE_FONT);
 
-		JLabel statusFilterLbl = new JLabel("Status: ");
+		statusFilterLbl = new JLabel("Status: ");
 		statusFilterLbl.setFont(Style.BASE_FONT);
-		String[] s1 = new String[] {"", "Undecided", "Backlog", "Watching", "Completed", "Dropped"};
-		JComboBox<String> statusFilter = new JComboBox<String>(s1);
+		statusFilter = new JComboBox<String>(showStatus);
 		statusFilter.setFont(Style.BASE_FONT);
 
 		gbc.gridy = 4; // row 5
@@ -122,33 +178,45 @@ public class ListPage extends Page {
 
 		gbc.gridx = 2; // col 3
 		filterPanel.add(statusFilterLbl, gbc);
-		
+
 		gbc.gridx = 3; // col 4
 		filterPanel.add(statusFilter, gbc);
 
+	}
+
+	void addRatingSelectorButtons() {
 		// rating range row 2x JSpinners
-		JLabel minRatingLbl = new JLabel("Min Rating: ");
+		minRatingLbl = new JLabel("Min Rating: ");
 		minRatingLbl.setFont(Style.BASE_FONT);
-		JSpinner minRating = new JSpinner(new SpinnerNumberModel(0, 0, 10, 1));
+		minRating = new JSpinner(new SpinnerNumberModel(0, 0, 10, 1));
 		minRating.setFont(Style.BASE_FONT);
 
-		JLabel maxRatingLbl = new JLabel("Max Rating: ");
+		maxRatingLbl = new JLabel("Max Rating: ");
 		maxRatingLbl.setFont(Style.BASE_FONT);
-		JSpinner maxRating = new JSpinner(new SpinnerNumberModel(10, 0, 10, 1));
+		maxRating = new JSpinner(new SpinnerNumberModel(10, 0, 10, 1));
 		maxRating.setFont(Style.BASE_FONT);
 
+		// change listeners for both ratings
+		// adds the listeners that do the things to ensure bounds are set properly
+		// ex. min is always <= max
+		// and max is always >= min
 		minRating.addChangeListener(e -> {
+			// both cases pull the values by getting value casted to an int directly
+			// the spinners themselves only allow numbers due to the SpinnerNumberModel
 			int minVal = (int) minRating.getValue();
 			int maxVal = (int) maxRating.getValue();
 
+			// then check if min is greater than max
 			if (minVal > maxVal)
+				// in which case, max must match to go up
 				maxRating.setValue(minVal);
- 		});
+		});
 
 		maxRating.addChangeListener(e -> {
 			int minVal = (int) minRating.getValue();
 			int maxVal = (int) maxRating.getValue();
 
+			// in this case, min must follow max
 			if (maxVal < minVal)
 				minRating.setValue(maxVal);
 		});
@@ -166,11 +234,15 @@ public class ListPage extends Page {
 		gbc.gridx = 3; // col 4
 		filterPanel.add(maxRating, gbc);
 
+	}
+
+	void addSearchButton() {
 		// search button on set row
-		JButton searchButton = new JButton("Search");
+		searchButton = new JButton("Search");
 		searchButton.setFont(Style.BASE_FONT);
 		searchButton.addActionListener(e -> {
-			// TODO: Implement db. also verify if selector for name and status are blank to not care
+			// TODO: Implement db. also verify if selector for name and status are blank to
+			// not care
 			String nameToCheck = nameFilter.getText();
 			String statusToCheck = statusFilter.getSelectedItem().toString();
 			int minRatingToCheck = (int) minRating.getValue();
@@ -183,40 +255,42 @@ public class ListPage extends Page {
 
 		gbc.gridy = 6; // row 7
 		gbc.gridx = 0; // col 1
-		filterPanel.add(searchButton);
+		filterPanel.add(searchButton, gbc);
+	}
 
-
+	void createListPanel() {
 		// create list panel
 		listPanel = new JPanel();
 		listPanel.setBackground(PageColor);
 		listPanel.setLayout(new BorderLayout());
+	}
 
+	void addTableToListPanel() {
 		// create JTable to display on list panel
 		listTableModel = new DefaultTableModel(colNames, 0) {
 			@Override
 			public boolean isCellEditable(int row, int column) {
 				return false;
 			}
+
 			// allows for column 1 to be an icon
 			// images have to be pre-scaled or modified before.
 			// might have to modify row size
 			@Override
-            public Class<?> getColumnClass(int column) {
-                // Tell the table that column index 1 contains Icons
-                if (column == 1) {
-                    return Icon.class; 
-                }
-                return super.getColumnClass(column);
-            }
+			public Class<?> getColumnClass(int column) {
+				// Tell the table that column index 1 contains Icons
+				if (column == 1) {
+					return Icon.class;
+				}
+				return super.getColumnClass(column);
+			}
 		};
 		listTable = new JTable(listTableModel);
-		JScrollPane tableScrollContainer = new JScrollPane(listTable);
+		tableScrollContainer = new JScrollPane(listTable);
 		tableScrollContainer.setBorder(border);
 		tableScrollContainer.getViewport().setBackground(PageColor);
 
 		contentPanel.add(filterPanel, BorderLayout.WEST);
 		contentPanel.add(tableScrollContainer, BorderLayout.CENTER);
-
-		this.add(contentPanel);
 	}
 }
