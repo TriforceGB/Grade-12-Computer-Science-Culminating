58303e98734b40762287e6a857a3db85f12b694f
Michael <mrauliuc9@gmail.com>
2026-06-11 14:27:27 -0400

    Finished list page basic

--- src/UI/Pages/ListPage.java
+++ src/UI/Pages/ListPage.java
@@ -6,14 +6,18 @@ import UI.Style;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
-
+import javax.swing.SpinnerNumberModel;
 import javax.swing.BorderFactory;
 import javax.swing.Icon;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 import javax.swing.table.DefaultTableModel;
 
@@ -58,7 +62,8 @@ public class ListPage extends Page {
 
 		// add components to filter panel
 
-		// three checkboxes in a row (type) (all require a label attached)
+		// three checkboxes in three different rows (type) (all require a label attached)
+		// note row comments are not accurate (psa gridy = 0 -> row 1)
 		JLabel movieTypeLbl = new JLabel("Movie: ");
 		movieTypeLbl.setFont(Style.BASE_FONT);
 		JCheckBox movieType = new JCheckBox();
@@ -72,29 +77,114 @@ public class ListPage extends Page {
 		JCheckBox animeType = new JCheckBox();
 
 		gbc.gridx = 0; // col 1
-		gbc.gridy = 0; // row 1
-		gbc.insets = Style.LABEL_PADS;
+		gbc.gridy = 1; // row 1
+		//gbc.insets = Style.LABEL_PADS;
 		filterPanel.add(movieTypeLbl, gbc);
 
 		gbc.gridx = 1; // col 2
 		filterPanel.add(movieType, gbc);
 
-		gbc.gridx = 2; // col 3
+		gbc.gridx = 0; // col 1
+		gbc.gridy = 2; // row 2
 		filterPanel.add(showTypeLbl, gbc);
 
-		gbc.gridx = 3; // col 4
+		gbc.gridx = 1; // col 1
 		filterPanel.add(showType, gbc);
 
-		gbc.gridx = 4; // col5
+		gbc.gridx = 0; // col 1
+		gbc.gridy = 3; // row 3
 		filterPanel.add(animeTypeLbl, gbc);
 
-		gbc.gridx = 5; // col 6
+		gbc.gridx = 1; // col 2
 		filterPanel.add(animeType, gbc);
 
 		// name & status row
 		// name is texfield and status is a dropdown
+		JLabel nameFilterLbl = new JLabel("Name: ");
+		nameFilterLbl.setFont(Style.BASE_FONT);
+		JTextField nameFilter = new JTextField(10);
+		nameFilter.setSize(new Dimension(100, 10));
+		nameFilter.setFont(Style.BASE_FONT);
+
+		JLabel statusFilterLbl = new JLabel("Status: ");
+		statusFilterLbl.setFont(Style.BASE_FONT);
+		String[] s1 = new String[] {"", "Undecided", "Backlog", "Watching", "Completed", "Dropped"};
+		JComboBox<String> statusFilter = new JComboBox<String>(s1);
+		statusFilter.setFont(Style.BASE_FONT);
+
+		gbc.gridy = 4; // row 5
+		gbc.gridx = 0; // col 1
+		// insets still exist
+		filterPanel.add(nameFilterLbl, gbc);
+
+		gbc.gridx = 1; // col 2
+		filterPanel.add(nameFilter, gbc);
+
+		gbc.gridx = 2; // col 3
+		filterPanel.add(statusFilterLbl, gbc);
+		
+		gbc.gridx = 3; // col 4
+		filterPanel.add(statusFilter, gbc);
 
 		// rating range row 2x JSpinners
+		JLabel minRatingLbl = new JLabel("Min Rating: ");
+		minRatingLbl.setFont(Style.BASE_FONT);
+		JSpinner minRating = new JSpinner(new SpinnerNumberModel(0, 0, 10, 1));
+		minRating.setFont(Style.BASE_FONT);
+
+		JLabel maxRatingLbl = new JLabel("Max Rating: ");
+		maxRatingLbl.setFont(Style.BASE_FONT);
+		JSpinner maxRating = new JSpinner(new SpinnerNumberModel(10, 0, 10, 1));
+		maxRating.setFont(Style.BASE_FONT);
+
+		minRating.addChangeListener(e -> {
+			int minVal = (int) minRating.getValue();
+			int maxVal = (int) maxRating.getValue();
+
+			if (minVal > maxVal)
+				maxRating.setValue(minVal);
+ 		});
+
+		maxRating.addChangeListener(e -> {
+			int minVal = (int) minRating.getValue();
+			int maxVal = (int) maxRating.getValue();
+
+			if (maxVal < minVal)
+				minRating.setValue(maxVal);
+		});
+
+		gbc.gridy = 5; // row 6
+		gbc.gridx = 0; // col 1
+		filterPanel.add(minRatingLbl, gbc);
+
+		gbc.gridx = 1; // col 2
+		filterPanel.add(minRating, gbc);
+
+		gbc.gridx = 2; // col 3
+		filterPanel.add(maxRatingLbl, gbc);
+
+		gbc.gridx = 3; // col 4
+		filterPanel.add(maxRating, gbc);
+
+		// search button on set row
+		JButton searchButton = new JButton("Search");
+		searchButton.setFont(Style.BASE_FONT);
+		searchButton.addActionListener(e -> {
+			// TODO: Implement db. also verify if selector for name and status are blank to not care
+			String nameToCheck = nameFilter.getText();
+			String statusToCheck = statusFilter.getSelectedItem().toString();
+			int minRatingToCheck = (int) minRating.getValue();
+			int maxRatingToCheck = (int) maxRating.getValue();
+			boolean canBeMovie = movieType.isSelected();
+			boolean canBeShow = showType.isSelected();
+			boolean canBeAnime = animeType.isSelected();
+			System.out.println();
+		});
+
+		gbc.gridy = 6; // row 7
+		gbc.gridx = 0; // col 1
+		filterPanel.add(searchButton);
+
 
 		// create list panel
 		listPanel = new JPanel();
