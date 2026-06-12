ce385aa4c96243c0f00cceacd32437059bd24417
MimbalR <mrauliuc9@gmail.com>
2026-06-11 22:53:49 -0400

    I am lost

--- src/UI/Pages/SearchPage.java
+++ src/UI/Pages/SearchPage.java
@@ -1,6 +1,7 @@
 package UI.Pages;
 
 import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -10,9 +11,9 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.image.BufferedImage;
+import java.awt.Label;
+import java.net.URI;
 import java.net.URL;
-import java.util.concurrent.ExecutionException;
 
 import UI.Style;
 
@@ -35,13 +36,15 @@ public class SearchPage extends Page {
 	private JLabel searchLbl;
 	private JTextField searchField;
 	private JComboBox<String> searchTypeBox;
-	private final String[] types = new String[] {"Movie", "TV Show", "Anime"};
+	private final String[] types = new String[] { "Movie", "TV Show", "Anime" };
 	private JButton searchBtn;
 
 	private JScrollPane listScrollContainer;
 
-	private final int posterWidth = 40;
-	private final int posterHeight = 80;
+	private final int posterWidth = 200;
+	private final int posterHeight = 200;
+
+	private final String testUrlString = "https://images.unsplash.com/photo-1450558415837-1f5e21a17709?ixid=M3w4MjcwNjd8MHwxfHNlYXJjaHwzfHxqZXN1c3xlbnwwfHx8fDE3ODEyMjk4ODh8MA&ixlib=rb-4.1.0&fit=max&q=80";
 
 	/**
 	 * Create the Search Page
@@ -64,6 +67,32 @@ public class SearchPage extends Page {
 		addListScrollContainer();
 
 		// testing for adding x number of panels with information
+		JPanel contentPanel = new JPanel();
+		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+		for (int i = 0; i < 3; i++) {
+			JPanel test = new JPanel();
+			// get url in try catch
+			URL url;
+			try {
+				url = new URI(testUrlString).toURL();
+			} catch (Exception e) {
+				url = null;
+			}
+			// attempt to create and scale poster
+			ImageIcon poster;
+			if (url != null)
+				poster = getSearchResultPoster(url);
+			else
+				poster = null;
+
+			// create poster
+			gbc.gridy = 0; // row is always 0
+			gbc.gridx = 0; // col 1
+			gbc.insets = new Insets(20, 20, 20, 20);
+			test.add(new JLabel("poster"));
+			contentPanel.add(test);
+		}
+		listScrollContainer.add(contentPanel);
 	}
 
 	void createContentPanel() {
@@ -78,20 +107,23 @@ public class SearchPage extends Page {
 		searchPanel = new JPanel();
 		searchPanel.setBackground(PageColor);
 		searchPanel.setLayout(new GridBagLayout());
+		gbc = new GridBagConstraints();
 
-		contentPanel.add(searchPanel);
+		contentPanel.add(searchPanel, BorderLayout.NORTH);
 	}
 
 	void createListPanel() {
 		listPanel = new JPanel();
 		listPanel.setBackground(PageColor);
 
-		contentPanel.add(listPanel);
+		contentPanel.add(listPanel, BorderLayout.CENTER);
 	}
 
 	void addSearchLabel() {
 		searchLbl = new JLabel("Search: ");
 		searchLbl.setFont(Style.BASE_FONT);
+
+		searchPanel.add(searchLbl);
 	}
 
 	void addSearchField() {
@@ -149,8 +181,27 @@ public class SearchPage extends Page {
 		result.setBackground(PageColor);
 		result.setLayout(new GridBagLayout());
 		gbc = new GridBagConstraints(); // reset gbc to ensure ready to go
-		
 
+		// get url in try catch
+		URL url;
+		try {
+			url = new URI(testUrlString).toURL();
+		} catch (Exception e) {
+			url = null;
+		}
+		// attempt to create and scale poster
+		ImageIcon poster;
+		if (url != null)
+			poster = getSearchResultPoster(url);
+		else
+			poster = null;
+
+		// create poster
+		gbc.gridy = 0; // row is always 0
+		gbc.gridx = 0; // col 1
+		gbc.insets = new Insets(20, 20, 20, 20);
+
+		result.add(new JLabel(poster));
 
 		return result;
 	}
@@ -158,9 +209,8 @@ public class SearchPage extends Page {
 	ImageIcon getSearchResultPoster(URL url) {
 		try {
 			return ui.resizeImg(new ImageIcon(ImageIO.read(url)), posterWidth, posterHeight);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			return null;
 		}
-	}		
+	}
 }
