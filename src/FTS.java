a202042b3749f725d2ee78e280978d1d8daa7c23
Zachary Schultz <zach.schultz.2.0.1@gmail.com>
2026-06-12 22:57:50 -0400

    removing test code

--- src/FTS.java
+++ src/FTS.java
@@ -1,7 +1,5 @@
 import UI.UI;
 import DB.DB;
-import DTO.API.Response.TheTVDBSearchResponse.Data;
-import DTO.LocalDB.Media;
 import API.API;
 
 public class FTS {
@@ -16,14 +14,6 @@ public class FTS {
 	public static void main(String[] args) {
 		db = new DB(DB_PATH);
 		api = new API(db);
-		Data[] anime = api.searchShow("Lucky Star", 3);
-		Media[] media = new Media[anime.length];
-		for (int i = 0; i < anime.length; i++) {
-			media[i] = new Media(anime[i]);
-		}
-		for (Media media2 : media) {
-			System.out.println(media2.getName());
-		}
 		ui = new UI(db);
 	}
 }
