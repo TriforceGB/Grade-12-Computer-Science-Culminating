19593b9f1c7998a21160d650926b76e09a965185
MimbalR <mrauliuc9@gmail.com>
2026-06-18 12:28:28 -0400

    Ui commented

--- src/UI/UI.java
+++ src/UI/UI.java
@@ -57,6 +57,7 @@ public class UI extends JFrame implements EventListener {
 	private AdminUserPage adminUsrPage;
 	private AdminMediaPage adminMediaPage;
 
+	// these variables store load conditions to ensure unneed loading doesn't occur
 	private boolean loadedMediaPageOnce = false;
 	private boolean loadedAdminUserPage = false;
 	private boolean loadedAdminMediaPage = false;
@@ -105,6 +106,7 @@ public class UI extends JFrame implements EventListener {
 		this.panelContainer.add(this.adminUsrPage, "adminUsr");
 		this.panelContainer.add(this.adminMediaPage, "adminMedia");
 
+		// show the starting layout
 		this.card.show(this.panelContainer, "login"); // Show the Login Panel by Default
 
 		this.setVisible(true); // Display the Window
@@ -119,6 +121,7 @@ public class UI extends JFrame implements EventListener {
 	public void switchPanel(String panelName) {
 		this.card.show(this.panelContainer, panelName);
 
+		// additional checks to do loading and reset functions upon loading certain windows
 		if (panelName.equals("list") && !loadedMediaPageOnce) {
 			loadedMediaPageOnce = true;
 			callreset();
@@ -136,6 +139,7 @@ public class UI extends JFrame implements EventListener {
 		}
 	}
 
+	// a share point for the media page to open from any panel and send back to that panel
 	public void openMediaPage(Media ref, String panelNameCalledFrom) {
 		// setup page
 		mediaPage.setupMediaPanel(ref, panelNameCalledFrom);
@@ -748,6 +752,7 @@ public class UI extends JFrame implements EventListener {
 		this.homePage.createWidgets();
 	}
 
+	// a switch case to transform the int stored for status to a string id
 	public String getMeidaTypeFromInt(int movieType) {
 		switch (movieType) {
 			case 1:
@@ -761,6 +766,7 @@ public class UI extends JFrame implements EventListener {
 		}
 	}
 
+	// a switch case to transform the int stored in db for status to a string id
 	public String getStatusString(int status) {
 		switch (status) {
 			case 0:
@@ -778,6 +784,7 @@ public class UI extends JFrame implements EventListener {
 		}
 	}
 
+	// allowing public functions to formulate db tables
 	public boolean remakeMediaTable() {
 		db.remakeMediaDB();
 		return true;
